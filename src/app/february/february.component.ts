30778a8ce5cad2dc9d64eb16a3f16222dc668d0b
wilsonmike <michaelwilsonsj@hotmail.com>
2021-03-09 16:31:39 -0500

    added routing and month dropdown| adjust data routing on month

--- src/app/february/february.component.ts
+++ src/app/february/february.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./february.component.css']
 })
 export class FebruaryComponent implements OnInit {
+  getOrders: any = [];
+  term = '';
+  searchline = '';
+  lineitems: any = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onEdit(): any{
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
 
 }

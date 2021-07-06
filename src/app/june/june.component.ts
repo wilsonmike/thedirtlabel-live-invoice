1c7ded5df4e997b55f08570d1e9a724b54305c56
wilsonmike <michaelwilsonsj@hotmail.com>
2021-07-06 13:55:42 -0400

    rendering june data

--- src/app/june/june.component.ts
+++ src/app/june/june.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DatafetchService } from '../datafetch.service';
+import juneLineJson from '../juneline.json';
+import juneJsonJson from '../june.json';
 
 @Component({
   selector: 'app-june',
   templateUrl: './june.component.html',
-  styleUrls: ['./june.component.css']
+  styleUrls: ['./june.component.css'],
 })
 export class JuneComponent implements OnInit {
+  june: any = juneJsonJson;
+  juneline: any = juneLineJson;
+  term = '';
+  searchline = '';
 
-  constructor() { }
+  constructor(
+    private service: DatafetchService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onEdit(): any {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
 
+  getMayLine = () => {
+    //tslint:disable
+    this.juneline.sort((a, b) =>
+      b['Order - Number'] > a['Order - Number']
+        ? 1
+        : a['Order - Number'] > b['Order - Number']
+        ? -1
+        : 0
+    );
+  };
+
+  getMay = () => {
+    //tslint:disable
+    this.june.sort((a, b) =>
+      b['Order - Number'] > a['Order - Number']
+        ? 1
+        : a['Order - Number'] > b['Order - Number']
+        ? -1
+        : 0
+    );
+  };
 }
